4abc345713c120f9d69ca63978005267ad6233fa
thanne114 <aidillafathan1@gmail.com>
2026-04-14 08:52:28 +0800

    Upload proyek pw_fathan

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hello!";
+$nama = "Fathan";
+$umur = 20;
+$tinggi = 169;
+$hobi = ["berinvestasi"];
+
+echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi, hobi saya $hobi[0]";
+?>
