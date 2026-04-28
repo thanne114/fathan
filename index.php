303027bf3cd352c56f53e091b3239362e75f7418
Fathan <aidillafathan1@gmail.com>
2026-04-28 09:02:37 +0800

    tambah materi function

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     echo "<br><button><a href ='materi1.php'>Materi 1: Variabel dan Tipe Data</a></button>";
     echo "<br><button><a href ='materi2.php'>Materi 2: Kondisi dan Looping</a></button>";
-    echo "<br><button><a href ='materi3.php'>Materi 3: Array</a></button>";
-    echo "<br><button><a href ='materi4.php'>Materi 4: Fungsi</a></button>";
+    echo "<br><button><a href ='materi3.php'>Materi 3: Function</a></button>";
+    echo "<br><button><a href ='materi4.php'>Materi 4: Array</a></button>";
 ?> 
--- materi3.php
+++ materi3.php
@@ -1,1 +1,45 @@
-materi tentang function
+<?php
+
+function sapa($nama) {
+    echo "Halo, $nama!<br>";
+}
+
+sapa("Budi");
+sapa("Citra");
+sapa("Doni");
+
+
+echo "<br><br><b>Kalkulator Menggunakan Function</b><br>";
+function kalkulator($a, $b,$op){
+    if($op == "+") {
+        return $a + $b;
+    }
+    else if($op == "-") {
+        return $a - $b;
+    }
+    else if($op == "*") {
+        return $a * $b;
+    }
+    else if($op == "/") {
+        return $a / $b;
+    }
+}
+
+?>
+
+<form action="" method="post">
+    <input type="text" name="angka1">
+    <input type="text" name="op">
+    <input type="text" name="angka2">
+    <input type="submit" value="Hitung">
+</form>
+
+<?php
+if (isset($_POST['angka1']) && isset($_POST['angka2']) && isset($_POST['op'])) {
+    $angka1 = $_POST['angka1'];
+    $op = $_POST['op'];
+    $angka2 = $_POST['angka2'];
+    $hasil = kalkulator($angka1, $angka2, $op);
+    echo "Hasil: $hasil<br>";
+}   
+?>
